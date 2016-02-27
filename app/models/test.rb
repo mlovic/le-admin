6df23db3b271dcb9c99e41137a27592627cbe787
Marko Lovic <markolovic@hotmail.com>
2016-02-27 19:50:13 +0100

    add test list to groups#show

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,16 @@
 class Test < ActiveRecord::Base
   belongs_to :test_book
   has_and_belongs_to_many :groups
+
+  def name
+    test_book.name + ' Test ' + number.to_s
+  end
+
+  def book_id
+    test_book_id
+  end
+
+  def given_to?(group)
+    groups.map(&:id).include?(group.id)
+  end
 end
