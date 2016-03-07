536c128ccb13e62805c768813fb1692625c4b214
Marko Lovic <markolovic@hotmail.com>
2016-03-07 21:28:43 +0100

    update unicorn config to save things in le_admin/tmp

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,6 +1,6 @@
 # set path to application
 app_dir = File.expand_path("../../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
+#shared_dir = "#{app_dir}/shared"
 working_directory app_dir
 
 
@@ -10,11 +10,11 @@ working_directory app_dir
  timeout 30
 
 # Set up socket location
- listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+ listen "tmp/sockets/unicorn.sock", :backlog => 64
 
 # Logging
- stderr_path "#{shared_dir}/log/unicorn.stderr.log"
- stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+ stderr_path "log/unicorn.stderr.log"
+ stdout_path "log/unicorn.stdout.log"
 
 # Set master PID location
- pid "#{shared_dir}/pids/unicorn.pid"
+ pid "tmp/pids/unicorn.pid"
